257771e19df71f49b4af484667ebe87709abdfa8
anil kumar <anil.k@whide.com>
2021-08-28 13:01:45 +0530

    Move test logic to a separate class HTTPClientSpy

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -9,16 +9,25 @@ import XCTest
 
 class RemoteFeedLoader {
     func load() {
-        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+        //HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+        
+        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
     }
 }
 
 class HTTPClient {
-    static let shared = HTTPClient()
-    private init(){}
-    var requestedURL : URL?
+    static var shared = HTTPClient()
+    //private init(){}   // Reovme as this is not a singelton anymore.
+    func get(from url: URL) { }
 }
 
+//Test Specific
+class HTTPClientSpy : HTTPClient {
+    var requestedURL : URL?
+    override func get(from url: URL) {
+        requestedURL = url
+    }
+}
 
 class RemoteFeedLoaderTests: XCTestCase {
 
@@ -26,7 +35,10 @@ class RemoteFeedLoaderTests: XCTestCase {
         //Arrange
         //let clinet = HTTPClient()
         
-        let client = HTTPClient.shared
+        //let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
+        
         _ = RemoteFeedLoader()
         
         //Assert
@@ -35,7 +47,11 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_requestDataFromURL(){
         //let client = HTTPClient()
-        let client = HTTPClient.shared
+        //let client = HTTPClient.shared
+        
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
+        
         let sut = RemoteFeedLoader()
         sut.load()
         XCTAssertNotNil(client.requestedURL)
@@ -48,4 +64,10 @@ class RemoteFeedLoaderTests: XCTestCase {
     1. Implemented Singleton way of doing things by moveing away form dependency injection and adapting to concrete types.
  
     2. HTTP Client do not have any reason to be singelton we can have multiple HTTPClients
+ */
+
+
+/*
+  // Get rid of Singelton.
+     Move the test logic to subclass of HTTPClient
  */
