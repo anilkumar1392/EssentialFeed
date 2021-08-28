8a8abf40fd68c36026787e5af70a92382cac14c6
anil kumar <anil.k@whide.com>
2021-08-28 12:48:25 +0530

    Request data from url on load by singleton pattern

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,10 +8,14 @@
 import XCTest
 
 class RemoteFeedLoader {
-    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+    }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    private init(){}
     var requestedURL : URL?
 }
 
@@ -20,11 +24,28 @@ class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL(){
         //Arrange
+        //let clinet = HTTPClient()
+        
+        let client = HTTPClient.shared
         _ = RemoteFeedLoader()
-        let clinet = HTTPClient()
         
         //Assert
-        XCTAssertNil(clinet.requestedURL)
+        XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL(){
+        //let client = HTTPClient()
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        sut.load()
+        XCTAssertNotNil(client.requestedURL)
     }
 
 }
+
+
+/*
+    1. Implemented Singleton way of doing things by moveing away form dependency injection and adapting to concrete types.
+ 
+    2. HTTP Client do not have any reason to be singelton we can have multiple HTTPClients
+ */
