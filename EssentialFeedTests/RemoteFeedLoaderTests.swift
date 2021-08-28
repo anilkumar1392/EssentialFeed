1d298b355e85798514b049fdbb43fe4b42c3d866
anil kumar <anil.k@whide.com>
2021-08-28 13:15:33 +0530

    Replace class and subclass with protocol and composition

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,23 +8,44 @@
 import XCTest
 
 class RemoteFeedLoader {
+    
+    let client: HTTPClient
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
     func load() {
         //HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
         
-        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
+        //HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
+        
+        client.get(from: URL(string: "https://a-url.com")!)
     }
 }
 
+/*
 class HTTPClient {
-    static var shared = HTTPClient()
-    //private init(){}   // Reovme as this is not a singelton anymore.
+    // static var shared = HTTPClient()
+    
+    // private init(){}   // Remove as this is not a singelton anymore.
+    
     func get(from url: URL) { }
+} */
+
+protocol HTTPClient {
+    func get(from url: URL)
 }
 
 //Test Specific
 class HTTPClientSpy : HTTPClient {
     var requestedURL : URL?
+    
+    /*
     override func get(from url: URL) {
+        requestedURL = url
+    }*/
+    
+    func get(from url: URL) {
         requestedURL = url
     }
 }
@@ -36,10 +57,13 @@ class RemoteFeedLoaderTests: XCTestCase {
         //let clinet = HTTPClient()
         
         //let client = HTTPClient.shared
-        let client = HTTPClientSpy()
-        HTTPClient.shared = client
         
-        _ = RemoteFeedLoader()
+        //let client = HTTPClientSpy()
+        //HTTPClient.shared = client
+        
+        
+        let client = HTTPClientSpy()
+        _ = RemoteFeedLoader(client: client)
         
         //Assert
         XCTAssertNil(client.requestedURL)
@@ -47,12 +71,15 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_requestDataFromURL(){
         //let client = HTTPClient()
+        
         //let client = HTTPClient.shared
         
-        let client = HTTPClientSpy()
-        HTTPClient.shared = client
+        //let client = HTTPClientSpy()
+        //HTTPClient.shared = client
         
-        let sut = RemoteFeedLoader()
+        let client = HTTPClientSpy()
+        
+        let sut = RemoteFeedLoader(client: client)
         sut.load()
         XCTAssertNotNil(client.requestedURL)
     }
@@ -68,6 +95,18 @@ class RemoteFeedLoaderTests: XCTestCase {
 
 
 /*
-  // Get rid of Singelton.
+  //  Get rid of Singelton.
      Move the test logic to subclass of HTTPClient
+ */
+
+
+/*
+  Using Singelton we can write testable code but we can even write better.
+  We can use composition rather than Inheritacne.
+  Usign this we dont need shared instacne any more.
+  
+  //Less code btter control, we need to care about less responsibility.
+  We do not need to inject a shared instance any more.
+  Our Client do not need to locate that instance any more.
+  They can just use whatever is given.
  */
